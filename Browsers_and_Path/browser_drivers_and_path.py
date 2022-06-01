fbed68cede125bce3fa9ec834c3833e0b74698f1
yanichik <brunshte@gmail.com>
2022-06-01 16:01:24 -0700

    testing drivers by browser

--- Browsers_and_Path/browser_drivers_and_path.py
+++ Browsers_and_Path/browser_drivers_and_path.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 # Option 1: NOT recommended because code not portable
-driver = webdriver.Chrome(executable_path="/Users/yanichik/Downloads/chromedriver")
+# ALSO: will get WARNING:
+# DeprecationWarning: executable_path has been deprecated, please pass in a Service object
+# driver = webdriver.Chrome(executable_path="/Users/yanichik/Downloads/chromedriver")
+
+# Option 1a with selenium 4: using service object per warning from option 1 above
+# service_object = Service(executable_path="/Users/yanichik/Downloads/chromedriver")
+
+# Option 2: add executable to system path
+driver = webdriver.Chrome()
 
 # opens window to google.com
 driver.get("https://google.com")
---
+++ Browsers_and_Path/supported_drivers.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Chrome
+# driver = webdriver.Chrome()
+# driver.get("http://demostore.supersqa.com")
+
+# Safari
+driver = webdriver.Safari()
+driver.get("http://demostore.supersqa.com")
